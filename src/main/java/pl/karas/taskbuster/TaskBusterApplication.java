e6759bb896456dc70dbd373e1e3c0e9bacc32d65
Patryk Karas <patryk1karas@gmail.com>
2021-04-25 21:26:29 +0200

    initial commit

---
+++ src/main/java/pl/karas/taskbuster/TaskBusterApplication.java
@@ -0,0 +1,13 @@
+package pl.karas.taskbuster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskBusterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/karas/taskbuster/TaskBusterApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.karas.taskbuster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBusterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
