ce46b8b0920490e18a1d27e31ddec0a06ccf81ea
Patryk Karas <patryk1karas@gmail.com>
2021-09-09 14:41:37 +0200

    Update RegisterController

--- src/main/java/pl/karas/taskbuster/controller/RegisterController.java
+++ src/main/java/pl/karas/taskbuster/controller/RegisterController.java
@@ -3,7 +3,6 @@ package pl.karas.taskbuster.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,11 +36,11 @@ public class RegisterController {
             } else {
                 return ResponseEntity.badRequest().body("Authority is required");
             }
-
             userService.saveUser(user);
             return ResponseEntity.ok("User registered!");
         }
-        
+        return ResponseEntity.badRequest().body("Something went wrong !");
+
     }
 
 
